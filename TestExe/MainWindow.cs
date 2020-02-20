e00d5f83b99fc28a0ec7ecdac024ddb9c886bcb6
Tony <laomms@gmail.com>
2020-02-21 06:41:25 +0700

    Add files via upload

---
+++ TestExe/MainWindow.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestExe
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MainWindow_Load(object sender, EventArgs e)
+        {
+            this.Text = System.Diagnostics.Process.GetCurrentProcess().Id.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TestFunction("test");
+        }
+        private static void TestFunction(string msg)
+        {
+            MessageBox.Show(msg);
+        }
+
+    }
+}
